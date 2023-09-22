53baa554470866f05b2bd1de2fb19830b385b111
JonnyAcosta <Jonathan.JA804@gmail.com>
2023-09-22 00:20:17 -0600

    added new code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is your magic number? ");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,11);
     }
 }
