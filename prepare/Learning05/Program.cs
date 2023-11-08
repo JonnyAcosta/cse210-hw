b0f0e79cc7707ad249672abb5abe0de5cd076789
JonnyAcosta <Jonathan.JA804@gmail.com>
2023-11-08 16:02:53 -0700

    added code

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Dynamic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square s1 = new Square("Red", 3);
+        shapes.Add(s1);
+
+        foreach (Shape s in shapes)
+        {
+            string color =s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
